281333f434e9aa9f5277d2779c38405f887ade1b
0d-fako <od.fakorede@gmail.com>
2025-05-29 16:36:51 +0100

    MS Library Management System

--- catalogue/models.py
+++ catalogue/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = ""
+    summary = models.TextField()
+    isbn = models.CharField(max_length=13, unique=True)
+    genre = ""
+
+
+
