db2b4679f7f8ee6a8f8389328294e405ec4b5e08
0d-fako <od.fakorede@gmail.com>
2025-06-16 11:37:00 +0100

    django moshi library  ms

--- user/models.py
+++ user/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=11, unique=True)
+
+
+
+
+
+class Author(User):
+    dob = models.DateField(blank=False, null=False)
+    dod = models.DateField(blank=False, null=False)
