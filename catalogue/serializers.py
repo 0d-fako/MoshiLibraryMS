c4db4b7beb32dbd76fd234efac8a838005845b86
0d-fako <od.fakorede@gmail.com>
2025-06-17 18:10:37 +0100

    django moshi library  ms

--- catalogue/serializers.py
+++ catalogue/serializers.py
@@ -2,13 +2,20 @@ from pyexpat import model
 from rest_framework import serializers
 
 from catalogue.models import Book
+from user.models import Author
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = ['first_name', 'last_name']
 
 
 class BookSerializer(serializers.ModelSerializer):
+    author = AuthorSerializer(many=True, read_only=True)
     class Meta:
         model = Book
         fields = ("id" ,"title", "summary","author")
-
 
 
 
