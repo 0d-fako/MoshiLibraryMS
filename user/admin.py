6517988e439169d76eeab87d1aa7d8eabea5869d
0d-fako <od.fakorede@gmail.com>
2025-06-17 11:42:39 +0100

    django moshi library  ms

--- user/admin.py
+++ user/admin.py
@@ -1,11 +1,20 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 from . import models
 
 # Register your models here.
+
+
+@admin.register(models.User)
+class UserAdmin(BaseUserAdmin):
+    pass
+
+
+
+
 @admin.register(models.Author)
-class AuthorAdmin(UserAdmin):
+class AuthorAdmin(BaseUserAdmin):
     add_fieldsets = (
         (
             None,
@@ -15,3 +24,6 @@ class AuthorAdmin(UserAdmin):
             },
         ),
     )
+    list_display = ("first_name","last_name","email","phone_number","dob","dod" )
+    list_display_links = ("email","phone_number","dod" )
+    list_editable = ("first_name","last_name","dob")
