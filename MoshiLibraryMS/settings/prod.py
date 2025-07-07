05e4d1fb39bf19ab8bd0fdc57f415d6dea0fb520
0d-fako <od.fakorede@gmail.com>
2025-07-07 16:37:52 +0100

    Worked on production environment

---
+++ MoshiLibraryMS/settings/prod.py
@@ -0,0 +1,15 @@
+from common import *
+
+
+DEBUG = False
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv("DB_NAME"),
+        'USER' : os.getenv("DB_USER"),
+        'PASSWORD':os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"),
+    }
+}
